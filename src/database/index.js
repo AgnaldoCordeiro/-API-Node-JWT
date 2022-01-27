ffaf82b2907499e172542c0014eed90ada410ae4
AgnaldoCordeiro <agske13@gmail.com>
2022-01-27 09:32:29 -0300

    arrumando dotenv

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv/config');
 
-const urlBase = process.env.MONGODB_URI;
+const urlBase = process.env.MONGODB_URL
 mongoose.connect(urlBase);
 mongoose.Promise = global.Promise
 module.exports = mongoose;
